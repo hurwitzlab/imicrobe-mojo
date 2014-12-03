cf6b6b78bef8b1863a4bdf600fd5fac22e0b5556
Ken Youens-Clark <kyclark@email.arizona.edu>
2014-12-03 09:39:13 -0700

    not a fat controller

---
+++ lib/IMicrobe/Controller/ProjectPage.pm
@@ -0,0 +1,38 @@
+package IMicrobe::Controller::ProjectPage;
+
+use strict;
+use IMicrobe::DB;
+use Mojo::Base 'Mojolicious::Controller';
+
+# ----------------------------------------------------------------------
+sub view {
+    my $self  = shift;
+    my $pp_id = $self->param('project_page_id');
+    my $db    = IMicrobe::DB->new;
+    my $Page  = $db->schema->resultset('ProjectPage')->find($pp_id);
+
+    if (!$Page) {
+        return $self->reply->exception("Bad project page id ($pp_id)");
+    }
+
+    $self->respond_to(
+        json => sub {
+            $self->render( json => $Page );
+        },
+
+        html => sub {
+            $self->layout('default');
+
+            $self->render( 
+                title => sprintf("Project Page: %s", $Page->title),
+                page  => $Page,
+            );
+        },
+
+        txt => sub {
+            $self->render( text => dump($Page) );
+        },
+    );
+}
+
+1;
